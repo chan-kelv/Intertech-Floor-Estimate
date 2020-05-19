7934dc250f05785ff509fceb5e1bdf242b4775b4
Kelvin Chan <chan.kelv@gmail.com>
2020-05-18 23:48:59 -0700

    Added firebase init

---
+++ app/src/main/java/com/intertech/floorestimator/FloorEstimatorApplication.kt
@@ -0,0 +1,12 @@
+package com.intertech.floorestimator
+
+import android.app.Application
+import timber.log.Timber
+
+class FloorEstimatorApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
